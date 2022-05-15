7de8b0a27a5b9ce1f811b5ccaebc30a21c3e9e65
pdmxdd <pdmxdd@gmail.com>
2022-05-14 22:52:26 -0500

    moved producer to actual main, updated key and string props

--- earthquake-event-producer/src/main/scala/dev/paulmatthews/App.scala
+++ earthquake-event-producer/src/main/scala/dev/paulmatthews/App.scala
@@ -1,15 +1,27 @@
 package dev.paulmatthews
 
-/**
- * @author ${user.name}
- */
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
+
+import java.util.Properties
+
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    val properties = new Properties
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    properties.setProperty(ProducerConfig.ACKS_CONFIG, "1")
+//    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+//    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    val producer = new KafkaProducer[String, String](properties)
+
+    val record = new ProducerRecord[String, String]("geo-events", "2022-05-14T03:02:28.795Z,60.1376,-151.2598,55.4,1.3,ml,,,,0.3,ak,ak02265ntan3,2022-05-14T03:12:39.241Z,12 km SE of Clam Gulch, Alaska,earthquake,,2.3,,,automatic,ak")
+
+    producer.send(record)
+
+    producer.close()
   }
 
 }
