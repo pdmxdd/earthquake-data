537c4c05c04235c17279c674afcccfa2c347174f
pdmxdd <pdmxdd@gmail.com>
2022-05-14 23:38:36 -0500

    read CSV and send to topic

--- earthquake-event-producer/src/main/scala/dev/paulmatthews/App.scala
+++ earthquake-event-producer/src/main/scala/dev/paulmatthews/App.scala
@@ -4,9 +4,10 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, Produce
 import org.apache.kafka.common.serialization.StringSerializer
 
 import java.util.Properties
+import scala.io.Source
 
 object App {
-  
+
   def main(args : Array[String]) {
     val properties = new Properties
     properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
@@ -15,11 +16,16 @@ object App {
     properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
 //    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
     properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+
     val producer = new KafkaProducer[String, String](properties)
 
-    val record = new ProducerRecord[String, String]("geo-events", "2022-05-14T03:02:28.795Z,60.1376,-151.2598,55.4,1.3,ml,,,,0.3,ak,ak02265ntan3,2022-05-14T03:12:39.241Z,12 km SE of Clam Gulch, Alaska,earthquake,,2.3,,,automatic,ak")
+    val bufferedSource = Source.fromFile("/home/paul/.earthquake-data/exactly-once-eq-data.csv")
+    bufferedSource.getLines().foreach(line => {
+      val record = new ProducerRecord[String, String]("geo-events", line)
+      producer.send(record)
+    })
 
-    producer.send(record)
+    bufferedSource.close()
 
     producer.close()
   }
